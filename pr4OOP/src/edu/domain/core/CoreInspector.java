0105c73f7bb09fa15bf88f28cfff0489fbf9b037
KarpatskiDemon <kuzevjeki4@gmail.com>
2026-03-08 20:36:51 +0200

    Delete pr4OOP directory

--- pr4OOP/src/edu/domain/core/CoreInspector.java
+++
@@ -1,9 +0,0 @@
-package edu.domain.core;
-
-public class CoreInspector {
-    public static void inspect(Person p, LearningUnit u, Payment pay) {
-        p.logInternal("person");
-        u.prepare();
-        pay.coreAudit(pay.paymentId);
-    }
-}
